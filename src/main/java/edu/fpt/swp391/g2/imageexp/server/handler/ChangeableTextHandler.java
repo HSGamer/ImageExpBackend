dc5179ddf8dfd1f9c0c7466e8d05e275bc48ab71
hsgamer <huynhqtienvtag@gmail.com>
2021-05-26 11:13:54 +0700

    test json

--- src/main/java/edu/fpt/swp391/g2/imageexp/server/handler/ChangeableTextHandler.java
+++ src/main/java/edu/fpt/swp391/g2/imageexp/server/handler/ChangeableTextHandler.java
@@ -1,8 +1,10 @@
 package edu.fpt.swp391.g2.imageexp.server.handler;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.Setter;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,9 +15,16 @@ public class ChangeableTextHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        httpExchange.sendResponseHeaders(200, text.length());
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("content-type", "application/json");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("text", text);
+        byte[] bytes = jsonObject.toString().getBytes();
+
+        httpExchange.sendResponseHeaders(200, bytes.length);
         OutputStream outputStream = httpExchange.getResponseBody();
-        outputStream.write(text.getBytes());
+        outputStream.write(bytes);
         outputStream.flush();
         outputStream.close();
     }
