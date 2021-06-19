0bb50455dda7be9add18462dd3b239161fbde9b7
Huynh Tien <huynhqtienvtag@gmail.com>
2021-06-20 00:07:03 +0700

    add getStringListFromJsonValue

--- src/main/java/edu/fpt/swp391/g2/imageexp/utils/Utils.java
+++ src/main/java/edu/fpt/swp391/g2/imageexp/utils/Utils.java
@@ -9,6 +9,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * The utility class for general purposes
@@ -58,23 +60,47 @@ public class Utils {
     }
 
     /**
+     * Get the value list from the json value
+     *
+     * @param jsonValue      the json value
+     * @param checkPredicate the check-value predicate
+     * @param getFunction    the get-value function
+     * @param <T>            the type of the value
+     * @return the value list
+     */
+    public static <T> List<T> getListFromJsonValue(JsonValue jsonValue, Predicate<JsonValue> checkPredicate, Function<JsonValue, T> getFunction) {
+        List<T> list = new ArrayList<>();
+        if (checkPredicate.test(jsonValue)) {
+            list.add(getFunction.apply(jsonValue));
+        } else if (jsonValue.isArray()) {
+            JsonArray jsonArray = jsonValue.asArray();
+            for (JsonValue value : jsonArray) {
+                if (checkPredicate.test(value)) {
+                    list.add(getFunction.apply(value));
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
      * Get the integer list from the json value
      *
      * @param jsonValue the json value
      * @return the integer list
      */
     public static List<Integer> getIntListFromJsonValue(JsonValue jsonValue) {
-        List<Integer> integerList = new ArrayList<>();
-        if (jsonValue.isNumber()) {
-            integerList.add(jsonValue.asInt());
-        } else if (jsonValue.isArray()) {
-            JsonArray jsonArray = jsonValue.asArray();
-            for (JsonValue value : jsonArray) {
-                if (jsonValue.isNumber()) {
-                    integerList.add(value.asInt());
-                }
-            }
-        }
-        return integerList;
+        return getListFromJsonValue(jsonValue, JsonValue::isNumber, JsonValue::asInt);
     }
+
+    /**
+     * Get the string list from the json value
+     *
+     * @param jsonValue the json value
+     * @return the string list
+     */
+    public static List<String> getStringListFromJsonValue(JsonValue jsonValue) {
+        return getListFromJsonValue(jsonValue, JsonValue::isString, JsonValue::asString);
+    }
+
 }
