12b2b12d6a905ad222b433551778582b66092fe3
thanhdanh2401 <lephamthanhdanh@gmail.com>
2021-06-27 19:40:28 +0700

    Get and Set Verify State

---
+++ src/main/java/edu/fpt/swp391/g2/imageexp/command/user/GetVerifyStateCommand.java
@@ -0,0 +1,36 @@
+package edu.fpt.swp391.g2.imageexp.command.user;
+
+import edu.fpt.swp391.g2.imageexp.command.Command;
+import edu.fpt.swp391.g2.imageexp.entity.User;
+import edu.fpt.swp391.g2.imageexp.processor.UserProcessor;
+import org.apache.logging.log4j.Level;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class GetVerifyStateCommand extends Command {
+    public GetVerifyStateCommand() {
+        super("get-verify-state");
+    }
+
+    @Override
+    public void runCommand(String argument) {
+        Optional<User> optionalUser;
+        try {
+            optionalUser = UserProcessor.getUserByEmail(argument);
+        } catch (SQLException e) {
+            getLogger().log(Level.WARN, "There is an SQL exception when getting data", e);
+            return;
+        }
+        if (!optionalUser.isPresent()) {
+            getLogger().warn("That email doesn't exist");
+            return;
+        }
+        getLogger().info(optionalUser.get().getVerified());
+    }
+
+    @Override
+    public String getUsage() {
+        return super.getUsage() + " <email>";
+    }
+}
