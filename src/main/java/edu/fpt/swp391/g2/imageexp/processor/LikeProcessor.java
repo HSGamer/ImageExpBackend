17892ce11e4b89df13b46a1cd72bdce05e35724f
Huynh Tien <huynhqtienvtag@gmail.com>
2021-07-03 20:35:14 +0700

    Finish LikeProcessor

--- src/main/java/edu/fpt/swp391/g2/imageexp/processor/LikeProcessor.java
+++ src/main/java/edu/fpt/swp391/g2/imageexp/processor/LikeProcessor.java
@@ -1,10 +1,12 @@
 package edu.fpt.swp391.g2.imageexp.processor;
 
+import edu.fpt.swp391.g2.imageexp.database.DatabaseConnector;
 import edu.fpt.swp391.g2.imageexp.entity.Like;
+import me.hsgamer.hscore.database.client.sql.PreparedStatementContainer;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,7 +39,27 @@ public class LikeProcessor {
      * @throws SQLException if there is an SQL exception
      */
     public static void toggleLike(int userId, int postId) throws SQLException {
-        // TODO
+        if (isLiked(userId, postId)) {
+            try (
+                    PreparedStatementContainer container = PreparedStatementContainer.of(
+                            DatabaseConnector.getConnection(),
+                            "delete * from likes where userID = ? and postID = ?",
+                            userId, postId
+                    )
+            ) {
+                container.update();
+            }
+        } else {
+            try (
+                    PreparedStatementContainer container = PreparedStatementContainer.of(
+                            DatabaseConnector.getConnection(),
+                            "insert into likes(userID, postID) values (?, ?)",
+                            userId, postId
+                    )
+            ) {
+                container.update();
+            }
+        }
     }
 
     /**
@@ -49,8 +71,16 @@ public class LikeProcessor {
      * @throws SQLException if there is an SQL exception
      */
     public static boolean isLiked(int userId, int postId) throws SQLException {
-        // TODO
-        return false;
+        try (
+                PreparedStatementContainer container = PreparedStatementContainer.of(
+                        DatabaseConnector.getConnection(),
+                        "select * from likes where userID = ? and postID = ? limit 1",
+                        userId, postId
+                );
+                ResultSet resultSet = container.query()
+        ) {
+            return resultSet.next();
+        }
     }
 
     /**
@@ -61,8 +91,20 @@ public class LikeProcessor {
      * @throws SQLException if there is an SQL exception
      */
     public static List<Integer> getLikedPostIds(int userId) throws SQLException {
-        // TODO
-        return Collections.emptyList();
+        try (
+                PreparedStatementContainer container = PreparedStatementContainer.of(
+                        DatabaseConnector.getConnection(),
+                        "select postID from likes where userID = ?",
+                        userId
+                );
+                ResultSet resultSet = container.query()
+        ) {
+            List<Integer> list = new ArrayList<>();
+            while (resultSet.next()) {
+                list.add(resultSet.getInt("postID"));
+            }
+            return list;
+        }
     }
 
     /**
@@ -73,8 +115,19 @@ public class LikeProcessor {
      * @throws SQLException if there is an SQL exception
      */
     public static int countLikes(int postId) throws SQLException {
-        // TODO
-        return 0;
+        try (
+                PreparedStatementContainer container = PreparedStatementContainer.of(
+                        DatabaseConnector.getConnection(),
+                        "select COUNT(userID) from likes where postID = ?",
+                        postId
+                );
+                ResultSet resultSet = container.query()
+        ) {
+            if (!resultSet.next()) {
+                return 0;
+            }
+            return resultSet.getInt(1);
+        }
     }
 
     /**
@@ -85,7 +138,19 @@ public class LikeProcessor {
      * @throws SQLException if there is an SQL exception
      */
     public static List<Like> getLikes(int postId) throws SQLException {
-        // TODO
-        return Collections.emptyList();
+        try (
+                PreparedStatementContainer container = PreparedStatementContainer.of(
+                        DatabaseConnector.getConnection(),
+                        "select * from likes where postID = ?",
+                        postId
+                );
+                ResultSet resultSet = container.query()
+        ) {
+            List<Like> list = new ArrayList<>();
+            while (resultSet.next()) {
+                list.add(getLike(resultSet));
+            }
+            return list;
+        }
     }
 }
