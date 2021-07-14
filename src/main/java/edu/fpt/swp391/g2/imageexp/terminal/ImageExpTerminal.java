ca5f05d2c45c9e8b6a35ff719b81ec5e4113c14a
Huynh Tien <huynhqtienvtag@gmail.com>
2021-07-14 17:29:13 +0700

    no line reader for dumb terminal

--- src/main/java/edu/fpt/swp391/g2/imageexp/terminal/ImageExpTerminal.java
+++ src/main/java/edu/fpt/swp391/g2/imageexp/terminal/ImageExpTerminal.java
@@ -7,6 +7,7 @@ import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.UserInterruptException;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.terminal.impl.DumbTerminal;
 
 import java.io.IOException;
 
@@ -20,26 +21,38 @@ public class ImageExpTerminal {
     private Terminal terminal;
     private LineReader lineReader;
 
+    /**
+     * Init the terminal
+     *
+     * @throws IOException if there is an I/O error
+     */
     public void init() throws IOException {
         terminal = TerminalBuilder.builder()
                 .system(true)
                 .jansi(true)
                 .build();
-        lineReader = LineReaderBuilder.builder()
-                .terminal(terminal)
-                .build();
-        lineReader.setOpt(LineReader.Option.DISABLE_EVENT_EXPANSION);
-        lineReader.unsetOpt(LineReader.Option.INSERT_TAB);
+        if (!(terminal instanceof DumbTerminal)) {
+            lineReader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .build();
+            lineReader.setOpt(LineReader.Option.DISABLE_EVENT_EXPANSION);
+            lineReader.unsetOpt(LineReader.Option.INSERT_TAB);
+        }
     }
 
+    /**
+     * Start the line reader
+     */
     public void start() {
         while (isRunning()) {
             try {
-                String command = lineReader.readLine("> ");
-                if (command == null) {
-                    break;
+                if (lineReader != null) {
+                    String command = lineReader.readLine("> ");
+                    if (command == null) {
+                        break;
+                    }
+                    runCommand(command);
                 }
-                runCommand(command);
             } catch (UserInterruptException e) {
                 shutdown();
             } catch (EndOfFileException e) {
@@ -48,10 +61,20 @@ public class ImageExpTerminal {
         }
     }
 
+    /**
+     * Check if the server is running
+     *
+     * @return true if it is
+     */
     private boolean isRunning() {
         return !INSTANCE.isShuttingDown();
     }
 
+    /**
+     * Execute the command
+     *
+     * @param command the command
+     */
     private void runCommand(String command) {
         String[] split = command.split(" ", 2);
         if (!INSTANCE.getCommandManager().handleCommand(split[0], split.length > 1 ? split[1] : "")) {
@@ -59,6 +82,9 @@ public class ImageExpTerminal {
         }
     }
 
+    /**
+     * Shut down the server
+     */
     private void shutdown() {
         INSTANCE.getLogger().info("Shutting down!");
         INSTANCE.shutdown();
